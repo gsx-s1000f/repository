6369038ab016ec42bc6294cb9d91f4c7e1e26a31
gsx-s1000f <62490804+gsx-s1000f@users.noreply.github.com>
2024-08-25 20:18:52 +0900

    Create PowerShellHttp.ps1

---
+++ PowerShellHttp.ps1
@@ -0,0 +1,15 @@
+﻿$yyyymmdd = Get-Date -Format "yyyyMMdd"
+Start-Transcript (".\http" + $yyyymmdd + ".log") -Append
+
+$HostUrl = 'https://github.com'
+$LoginPath ='/gsx-s1000f'
+$HostUri = New-Object System.Uri($HostUrl)
+$Uri = New-Object System.Uri($HostUri, $LoginPath)
+try {
+    $res = Invoke-WebRequest -uri $Uri 
+
+    echo $res
+} catch {
+} finally {
+    Stop-Transcript
+}
